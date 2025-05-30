3f88c37185acad6186a44413b4568f64a1991f7b
Imran <imran889418@gmail.com>
2025-05-30 06:50:00 +0600

    first commit

---
+++ module 26/1-setting-up-development-environment/src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref } from 'vue';
+
+const text = ref("hello world");
+
+function changeText() {
+  text.value = "Good by world";
+}
+
+const user = {
+  name: 'Imran',
+  age: 25,
+}
+
+</script>
+
+<template>
+  <h1>{{ text }}</h1>
+  <button v-on:click="changeText">Submit</button>
+  <h1>{{ user.name }} - {{ user.age }}</h1>
+</template>
+
+<style scoped></style>
