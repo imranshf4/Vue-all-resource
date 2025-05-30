75bf3c1690cb29698298e954b077dd2afc4d7e92
Imran <imran889418@gmail.com>
2025-05-30 07:11:51 +0600

    2

--- module 26/1-setting-up-development-environment/src/components/HelloWorld.vue
+++ module 26/1-setting-up-development-environment/src/components/HelloWorld.vue
@@ -12,12 +12,16 @@ const user = {
   age: 25,
 }
 
+
+const count = ref(0)
 </script>
 
 <template>
   <h1>{{ text }}</h1>
   <button v-on:click="changeText">Submit</button>
-  <h1>{{ user.name }} - {{ user.age }}</h1>
+  <h1>{{ user.name }} - {{ user.age }} - {{ count }}</h1>
+   <button @click="count++">click </button>
+   <button v-if="count !== 0" @click="count--">click </button>
 </template>
 
 <style scoped></style>
